ddb4b73089b01e51872438240d204e3587483b80
Nikhil Reddy Vatti <35322884+sainikhil1605@users.noreply.github.com>
2021-10-17 14:38:03 +0530

    Added JWT token

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const UserModel = require('../models/UserModel');
 
 exports.LoginController = ((req, res) => {
@@ -9,8 +10,12 @@ exports.LoginController = ((req, res) => {
         res.status(401);
         res.send('Email or Password Does Not Match');
       }
-      res.status(200);
-      res.send(isPasswordMatch);
+      if (isPasswordMatch) {
+        res.status(200);
+        const token = jwt.sign({ userEmail: doc.userEmail, userPassword: doc.userPassword }, 'nikhil', { expiresIn: '2h' });
+        const isCorrect = jwt.verify(token, 'nikhil');
+        res.send(isCorrect);
+      }
     });
   });
 });
@@ -29,7 +34,7 @@ exports.SignUpController = ((req, res) => {
             res.send('Error Occured Please Try Again');
           } else {
             res.status(200);
-            res.send('Login Success');
+            res.send('Sign Up Success');
           }
         });
       });
