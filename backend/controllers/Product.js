568e2290885319906e1b5dfec4ac312ad9515052
Nikhil Reddy Vatti <35322884+sainikhil1605@users.noreply.github.com>
2021-12-09 12:23:07 +0530

    Added sorting limit and pagination features

--- backend/controllers/Product.js
+++ backend/controllers/Product.js
@@ -2,7 +2,16 @@ const { StatusCodes } = require('http-status-codes');
 const Product = require('../models/Product');
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find({});
+  const { sort } = req.query;
+  const result = Product.find({});
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result.sort(sortList);
+  }
+  const limit = Number(req.query.limit) || 10;
+  const page = Number(req.query.page) || 1;
+
+  const products = await result.skip((page - 1) * limit).limit(limit);
   if (!products) {
     throw new Error('Something went wrong');
   }
